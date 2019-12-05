1ceaa2c51e0bef1b46ae48383f71e08b7e7a9c13
Nikolai <nikolaikuhar95@gmail.com>
2019-12-05 12:22:52 +0300

    Modularity Maven2

---
+++ project-parent/сomics-web/src/main/java/by/it/academy/comics/ByeServlet.java
@@ -0,0 +1,21 @@
+package by.it.academy.comics;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ByeServlet", urlPatterns = "/saybye")
+public class ByeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h2>Bye it academy!</h2>");
+    }
+
+}
