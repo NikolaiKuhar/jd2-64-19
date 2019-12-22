f3fb159ec13ed54eb611aa1c70489809803a867c
Nikolai <nikolaikuhar95@gmail.com>
2019-12-22 18:40:59 +0300

    Task for tuesday - добавил hikaricp и flayway - добавил в листнер вызов миграции - добавил один файл sql для миграции базу создал для проекта

---
+++ project-parent/сomics-common/src/main/resources/db/migration/V1__Add_students.sql
@@ -0,0 +1,9 @@
+CREATE TABLE comics
+(
+    id          int          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    price       VARCHAR(20)  NOT NULL,
+    count       VARCHAR(10)  NOT NULL,
+    rating      VARCHAR(10)
+
+)
--- project-parent/сomics-web/src/main/java/by/it/academy/comics/web/listener/ComicsContextInitListener.java
+++ project-parent/сomics-web/src/main/java/by/it/academy/comics/web/listener/ComicsContextInitListener.java
@@ -2,10 +2,14 @@ package by.it.academy.comics.web.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import by.it.academy.comics.db.connection.ElDataSource;
+import by.it.academy.comics .db.migration.DbMigration;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 @WebListener()
 public class ComicsContextInitListener implements ServletContextListener {
@@ -15,6 +19,15 @@ public class ComicsContextInitListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         logger.info("Context initialized");
+        try {
+            ResourceBundle bundle = ResourceBundle.getBundle("mysql_hikari");
+            ElDataSource.configure(bundle);
+            DataSource dataSource = ElDataSource.getDataSource();
+            DbMigration.migrate(dataSource);
+        } catch (Exception e) {
+            logger.error("error", e);
+            throw new RuntimeException("Datasource initialisation error", e);
+        }
     }
 
     @Override
