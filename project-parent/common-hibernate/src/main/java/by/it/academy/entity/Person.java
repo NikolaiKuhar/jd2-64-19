70891322c50d95bc8f0b231a4814732a6e2ac89c
Nikolai <nikolaikuhar95@gmail.com>
2020-01-12 16:51:31 +0300

    Hibernate homework

---
+++ project-parent/common-hibernate/src/main/java/by/it/academy/entity/Person.java
@@ -0,0 +1,27 @@
+package by.it.academy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private Integer age;
+}
